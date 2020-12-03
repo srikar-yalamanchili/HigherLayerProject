af6ace520d9567dd85ade85580fa8655cf426b7b
srikar-yalamanchili <72405420+srikar-yalamanchili@users.noreply.github.com>
2020-12-03 18:06:44 -0500

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -7,11 +7,11 @@ var express_1 = __importDefault(require("express"));
 // import http from 'http';
 // import socket.io from 'socket.io';
 var app = express_1.default();
-var PORT = 5000;
+var PORT = 3000;
 var server = app.listen(PORT);
 var io = require('socket.io')(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5000",
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -28,7 +28,7 @@ var counter = 1;
 var clients = [];
 var auctionObjects = {};
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5000',
     credentials: true,
 }));
 app.use(express_1.default.json());
